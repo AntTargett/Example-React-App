1c34484156e2feba757bacc88f480ad3f0c9610c
Antony Targett <antony.targett@gmail.com>
2019-05-13 21:44:35 +1000

    fix/feat: added packages required for proper testing. Added example tests for App.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { render, fireEvent, cleanup, waitForElement } from "react-testing-library"
+import "jest-dom/extend-expect"
+import "react-testing-library/cleanup-after-each"
 import App from "./App"
 
-it("renders without crashing", () => {
-	const div = document.createElement("div")
-	ReactDOM.render(<App />, div)
-	ReactDOM.unmountComponentAtNode(div)
+describe("<App /> spec", () => {
+	it("App Renders without rashing", () => {
+		const div = document.createElement("div")
+		ReactDOM.render(<App />, div)
+		ReactDOM.unmountComponentAtNode(div)
+	})
+	it("renders table title", async () => {
+		const { getByText } = render(<App />)
+		//checks title exists
+		await waitForElement(() => getByText(/Pies of the day Total Results/))
+		//checks url link exists
+		await waitForElement(() => getByText(/Test Lazy loaded route/))
+	})
 })
